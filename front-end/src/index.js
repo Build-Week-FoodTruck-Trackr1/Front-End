b7a8095d7a30b21820e133a21c3cd5e62d5d0b81
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-12-18 20:13:19 -0700

    create redux store and initialize reducer/state

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer } from './reducers'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+<Router>
+    <Provider>
+        <App />
+    </Provider>
+</Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ front-end/src/reducers/index.js
@@ -0,0 +1,21 @@
+export const intitialState = {
+    diner: {
+        username: '',
+        password: '',
+        currentLocation: '',
+        favoriteTrucks: []
+    }, 
+
+    operator: {
+        username: '',
+        password: '',
+        trucksOwned: []
+    }
+}
+
+export const reducer = (state = intitialState, action) => {
+    switch(action.type) {
+        default: 
+            return state
+    }
+}
